460c385000bff1de38c552273b2f1f11e65a104b
Eric Denton <eric@iamericanartist.com>
2016-08-28 21:26:57 -0500

    first commit

---
+++ 09.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+"use strict"
+//////////////////// EXECUTE with "./09.js (fileToUseHere)" ////////////////////
+// ran "chmod +x 09.js" from TERMINAL to allow execution capabilities
+
+console.log("Linked!")
